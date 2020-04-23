87fef906e21df29462fa36c2353c498f97d3f732
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-04-23 21:40:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function happyHolidaysTo (name) {
 function happyCustomHolidayTo (holiday, name) {
   return (`Happy ${holiday}, ${name}!`)
 }
+function holidayCountdown (holiday, days) {
+  return (`It's ${days} days until ${holiday}!`)
+}
